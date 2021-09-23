1153e5c75d1e25f6bdd554d23a45cc5c22dbfb65
Jorgemariodiez <82427591+Jorgemariodiez@users.noreply.github.com>
2021-09-22 22:35:20 -0500

    Se crea menu

--- SeleccionColombia/src/seleccioncolombia/SeleccionColombia.java
+++ SeleccionColombia/src/seleccioncolombia/SeleccionColombia.java
@@ -13,6 +13,24 @@ public class SeleccionColombia {
         int seleccion;
         int contador=0;
         
+        do{
+            // Menu de opciones
+           System.out.println("Bienvenido");
+           System.out.println("---------------------");
+           System.out.println("1.Agregar jugador.");
+           System.out.println("2.Buscar jugador.");
+           System.out.println("3.Editar jugador.");
+           System.out.println("4.Mostrar seleccion.");
+           System.out.println("5.Salir");
+           System.out.println("---------------------");
+           System.out.println("");
+           
+           System.out.print("Digite una opcion");
+           seleccion = lea.nextInt();
+                   
+        }while(seleccion !=5);
+        
+        
         
         
         
