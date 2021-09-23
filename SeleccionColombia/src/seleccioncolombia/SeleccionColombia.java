edcff2df5298389b52976f34b40ffd290d27aafc
Jorgemariodiez <82427591+Jorgemariodiez@users.noreply.github.com>
2021-09-23 09:57:55 -0500

    Se crea menu 4

--- SeleccionColombia/src/seleccioncolombia/SeleccionColombia.java
+++ SeleccionColombia/src/seleccioncolombia/SeleccionColombia.java
@@ -1,7 +1,9 @@
 
 package seleccioncolombia;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 import jugadores.Jugadores;
 
 
@@ -34,16 +36,38 @@ public class SeleccionColombia {
         }while(seleccion !=5);
         
         switch (seleccion){
+            //Agregar
             case 1:
                 jugadores[contador]=new Jugadores();
+                System.out.println(" Digite el numero de la camiseta: ");
+                jugadores[contador].setNumCamiseta(lea.nextInt());
+                System.out.println(" Digite el nombre del jugador: ");
+                jugadores[contador].setNombre(lea.next());
+                System.out.println(" Digite el apellido del jugador: ");
+                jugadores[contador].setApellido(lea.next());
+                System.out.println(" Digite la posicion del jugador: ");
+                jugadores[contador].setPosicion(lea.next());
+                System.out.println(" Digite la edad del jugador: ");
+                jugadores[contador].setEdad(lea.nextInt());
+                System.out.println(" Digite el equipo de la liga donde juega: ");
+                jugadores[contador].setEquipoLiga(lea.next());
+                
+                contador++;
                 
                 break;
+                //Buscar
             case 2:
                 break;
+                //Editar
             case 3:
                 break;
+                //Listar
             case 4:
+                for(int i=0; i<contador; i++){
+                    System.out.println("-"+i+"--"+jugadores[i].getNombre()+" "+jugadores[i].getApellido());
+                }
                 break;
+                //Salir
             case 5:
                 System.out.println("Proceso Terminado");
                 System.exit(0);
