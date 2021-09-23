d76835230ac3b6691fbcbfa7a7e2580cd2113a65
Jorgemariodiez <82427591+Jorgemariodiez@users.noreply.github.com>
2021-09-22 20:25:06 -0500

    Se crea proyecto del taller 1 SeleccionColombia

---
+++ SeleccionColombia/src/seleccioncolombia/SeleccionColombia.java
@@ -0,0 +1,24 @@
+
+package seleccioncolombia;
+
+import java.util.Scanner;
+
+
+public class SeleccionColombia {
+
+    
+    public static void main(String[] args) {
+        Scanner lea = new Scanner(System.in);
+        
+        int seleccion;
+        int contador=0;
+        
+        Jugador[] jugador=new Jugador[23];
+        
+        
+        
+        
+       
+    }
+    
+}
